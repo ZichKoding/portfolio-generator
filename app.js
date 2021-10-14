f970e9c458fdd26823639ebf36b324f573bb0e9f
Chris Zichko <chriszichkocoding@gmail.com>
2021-10-14 10:41:37 -0400

    Modularized and finished the generate page.

--- app.js
+++ app.js
@@ -1,59 +1,11 @@
-// var message = "Hello Node!";
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-// var sum = 5 + 3;
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-// console.log(message);
-// console.log(sum);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
-
-// const message = 'Hello Node';
-// message = 'Goodbye Node';
-
-// const sum = 5 + 3;
-// sum = 2;
-
-// // console.log(message);
-// // console.log(sum);
-
-// const animalArray = ['dog', 'cat', 'pig'];
-
-// animalArray.push('cow');
-
-// const personObj = {
-//     name: 'Lernantino',
-//     age: 99
-// };
-
-// personObj.age = 100;
-// personObj.occupation = 'Developer';
-
-// console.log(animalArray);
-// console.log(personObj);
-
-let message = 'Hello Node!';
-message = 'Hello ES6!';
-
-let sum = 5 + 3;
-sum += 1;
-
-console.log(message + "\n" + sum);
-
-
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('======================');
-
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to the output!');
+});
